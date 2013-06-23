e282585c3c9cf2c1bcbb779c82b6f5b1a4fb564d
Stuart Lewis <stuart@stuartlewis.com>
2013-06-23 06:54:12 +0100

    Adding new unrestricted WorkflowManager that allows UPDATE and DELETE operations on items in workflow, in archive, and withdrawn

---
+++ dspace-swordv2/src/main/java/org/dspace/sword2/WorkflowManagerUnrestricted.java
@@ -0,0 +1,11 @@
+package org.dspace.sword2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: stuartlewis
+ * Date: 23/06/2013
+ * Time: 06:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class WorkflowManagerUnrestricted {
+}
